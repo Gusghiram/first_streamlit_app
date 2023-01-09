4dd08861acb05685d7cf7c4a77e26993b573de03
Gusghiram <55006343+Gusghiram@users.noreply.github.com>
2023-01-09 17:52:20 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('北 Omega 3 & Burberry Oatmeal')
+streamlit.text(':chicken: Omega 3 & Burberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
